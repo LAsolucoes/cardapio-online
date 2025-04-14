a4e88cb617df556c4a38c97e9b7ab5fa88453c21
Alexmoedo <117864509+LAsolucoes@users.noreply.github.com>
2025-04-14 07:41:11 -0300

    ListProdcts

--- src/app/_Components/Cardapio/index.tsx
+++ src/app/_Components/Cardapio/index.tsx
@@ -1,10 +1,61 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
-import ImageBurguer from "../../../../public/assets/burguer.png";
+
 
 import styles from "./cardapio.module.css";
+import { useEffect, useState } from "react";
 
+
+
+interface ListaProdutosProps {
+  id: string
+  img: string
+  name: string
+  dsc: string
+  price: number
+
+
+}
 export function Cardapio() {
+  const [addAmount, setAddAmount] = useState(0)
+  const [listProducts, setListProdducts] = useState<ListaProdutosProps[]>([])
+
+  const [ seeMore , setSeeMore ] = useState(false)
+
+  useEffect(() => {
+    async function ListProducts() {
+      try {
+        const response = await fetch("/dados.json")
+        const data = await response.json()
+        console.log(data)
+        setListProdducts(data)
+
+      } catch {
+        alert("errorrr")
+      }
+    }
+    ListProducts()
+  }, [])
+
+
+  function handleAdd() {
+    setAddAmount(addAmount + 1)
+  }
+
+  function handleRemove() {
+    if (!addAmount) {
+      setAddAmount(addAmount)
+      return
+    }
+    setAddAmount(addAmount - 1)
+  }
+
+
+  function SeeMore(){
+    alert("ok")
+  }
+
   return (
     <section className={styles.ContainerCardapio}>
       <div className={styles.TittleCardapio}>
@@ -12,7 +63,7 @@ export function Cardapio() {
         <h2>Conheça o nosso cardápio</h2>
       </div>
 
-      <div>
+      <div className={styles.ContainerCategory}>
         <nav className={styles.CategoryCardapio}>
           <Link href={"#"}>Burges</Link>
           <Link href={"#"}>Pizzas</Link>
@@ -22,22 +73,47 @@ export function Cardapio() {
           <Link href={"#"}>Sobremesas</Link>
         </nav>
       </div>
+      <div className={styles.CardapioContent}>
 
-      <div className={styles.ContentCardapioItens}>
-        <div className={styles.ItemCardapio}>
-          <div className={styles.ImageProduct}>
-              <Image
-                src={ImageBurguer}
-                alt="Imagem do Produto"
-                width={150}
-                height={100}
-              />
-          </div>
-          <div className={styles.NamePriceItem}>
-            <label className={styles.TitleProduct}>Nome do produto pode ser muito grande</label>
-            <label className={styles.PriceProduct}>R$ 9,99</label>
-          </div>
-        </div>
+
+        {listProducts.map(item => (
+
+          
+          <div className={styles.ContentCardapioItens} key={item.id}>
+              <div className={styles.ItemCardapio} >
+                <div className={styles.ImageProduct}>
+                  <Image
+                    src={item.img}
+                    alt={item.name}
+                    width={280}
+                    height={180}
+      
+                    className={styles.Image}
+                  />
+                </div>
+                <div className={styles.NamePriceItem}>
+                  <label className={styles.TitleProduct}>{item.name}</label>
+                  <p>{item.dsc}</p>
+                  <label className={styles.PriceProduct}>{`R$ ${item.price}`}</label>
+
+                  <div className={styles.ButtonAddRemove}>
+                    <button onClick={handleRemove}>-</button>
+                    <label> {addAmount} </label>
+                    <button onClick={handleAdd}>+</button>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+        
+
+        ))}
+
+
+
+
+
       </div>
     </section>
   );
