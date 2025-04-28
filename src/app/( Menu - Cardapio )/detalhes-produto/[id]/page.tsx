60728b002aa1c5d19ce4dcf8cb69801e36186fa5
Alex Moedo <alexmoedo@gmail.com>
2025-04-28 16:46:19 -0300

    category
    Ajustes nas categorias

--- src/app/( Menu - Cardapio )/detalhes-produto/[id]/page.tsx
+++ src/app/( Menu - Cardapio )/detalhes-produto/[id]/page.tsx
@@ -4,8 +4,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 import styles from "../../../( Menu - Cardapio )/cardapio.module.css";
-import { initScrollReveal } from "@/app/helpers/scrollReavealConfig";
+/*  import { initScrollReveal } from "@/app/helpers/scrollReavealConfig";  */
 import { useParams } from "next/navigation";
+
+interface AdditionalItens {
+  name: string;
+  price: number;
+}
 
 interface ProductProps {
   id: number;
@@ -15,18 +20,21 @@ interface ProductProps {
   ingredients: string[];
   price: number;
   category: number;
+  edges: AdditionalItens[];
+  additional: AdditionalItens[];
 }
 
 export default function DetalhesProduto() {
-  useEffect(() => {
+  /*      useEffect(() => {
     initScrollReveal();
-  }, []);
+  }, []); */
+
   const params = useParams();
   const elementId = Number(params.id);
-  console.log("params :", params);
-  console.log("elementoId :", elementId);
 
   const [listProduct, setListProduct] = useState<ProductProps | null>(null);
+  const [edges, setEdges] = useState<AdditionalItens | null>(null);
+  const [ additional , setAdditional] = useState<AdditionalItens | null>( null )
 
   useEffect(() => {
     async function listProductSelected() {
@@ -54,6 +62,13 @@ export default function DetalhesProduto() {
     setValueIncrementoDecremento((prev) => prev + 1);
     return;
   }
+
+  const totalPrice = listProduct
+    ? listProduct.price * valueIncremetoDecremento + (edges?.price ?? 0) + (additional?.price ?? 0)
+    : 0;
+
+  
+    
 
   return (
     <main className={styles.Main}>
@@ -109,67 +124,89 @@ export default function DetalhesProduto() {
                   </ul>
                 </div>
               )}
+
+              <div className={`${styles.Additional}`}>
+                {listProduct.edges && listProduct.edges.length > 0 && (
+                  <>
+                    <div className={styles.AdditionalTitle}>
+                      <h3>Deseja borda recheada?</h3>
+                      <label>Escolher até 1 opção</label>
+                    </div>
+
+                    <div className={styles.CardAdditional}>
+                      {listProduct.edges.map((edges, index) => (
+                        <>
+                          <div className={`${styles.OptionAdditional}`}>
+                            <label key={index}>{edges.name}</label>
+                            <div className={styles.OptionAdditionalPrice}>
+                              <label>
+                                {" "}
+                                +{" "}
+                                {edges.price.toLocaleString("pt-BR", {
+                                  style: "currency",
+                                  currency: "BRL",
+                                })}
+                              </label>
+                              <input
+                              name="additional"
+                                type="radio"
+                                onChange={() => setEdges(edges)}
+                                
+                              />
+                            </div>
+                          </div>
+                        </>
+                      ))}
+                    </div>
+                  </>
+                )}
+
+                {listProduct.additional &&
+                  listProduct.additional.length > 0 && (
+                    <>
+                      <div className={styles.AdditionalTitle}>
+                        <h3>Opcionais</h3>
+                        <label>Escolher até 1 opção</label>
+                      </div>
+
+                      <div className={styles.CardAdditional}>
+                        {listProduct.additional.map((additional, index) => (
+                          <>
+                            <div className={`${styles.OptionAdditional}`}>
+                              <label key={index}>{additional.name}</label>
+                              <div className={styles.OptionAdditionalPrice}>
+                                <label>
+                                  {" "}
+                                  +{" "}
+                                  {additional.price.toLocaleString("pt-BR", {
+                                    style: "currency",
+                                    currency: "BRL",
+                                  })}
+                                </label>
+                                <input type="checkbox"
+                                onChange={()=> setAdditional(additional)}
+                                />
+                              </div>
+                            </div>
+                          </>
+                        ))}
+                      </div>
+                    </>
+                  )}
+                <div className={styles.ContainerObservation}>
+                  <div className={styles.AdditionalTitle}>
+                    <h3>Observações</h3>
+                    <label>Informe alguma observação abaixa</label>
+                  </div>
+                  <div className={styles.CardAdditional}>
+                    <div className={`${styles.Observation} ${styles.Card}`}>
+                      <textarea className={styles.TextArea}></textarea>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </>
           )}
-
-          <div className={`${styles.Additional}`}>
-            <div className={styles.AdditionalTitle}>
-              <h3>Deseja borda recheada?</h3>
-              <label>Escolher até 1 opção</label>
-            </div>
-
-            <div className={styles.CardAdditional}>
-              <div className={`${styles.OptionAdditional}`}>
-                <label>Cheddar</label>
-                <div className={styles.OptionAdditionalPrice}>
-                  <label> + 8,99</label>
-                  <input type="radio" />
-                </div>
-              </div>
-
-              <div className={`${styles.OptionAdditional}`}>
-                <label>Catupiry</label>
-                <div className={styles.OptionAdditionalPrice}>
-                  <label> + 8,99</label>
-                  <input type="radio" />
-                </div>
-              </div>
-            </div>
-
-            <div className={styles.AdditionalTitle}>
-              <h3>Opcionais</h3>
-              <label>Escolher até 1 opção</label>
-            </div>
-
-            <div className={styles.CardAdditional}>
-              <div className={`${styles.OptionAdditional}`}>
-                <label>Queijo</label>
-                <div className={styles.OptionAdditionalPrice}>
-                  <label> + 5,00</label>
-                  <input type="radio" />
-                </div>
-              </div>
-
-              <div className={`${styles.OptionAdditional} ${styles.Card}`}>
-                <label>Azeitona</label>
-                <div className={styles.OptionAdditionalPrice}>
-                  <label> + 2,99</label>
-                  <input type="radio" />
-                </div>
-              </div>
-            </div>
-            <div className={styles.ContainerObservation}>
-              <div className={styles.AdditionalTitle}>
-                <h3>Observações</h3>
-                <label>Informe alguma observação abaixa</label>
-              </div>
-              <div className={styles.CardAdditional}>
-                <div className={`${styles.Observation} ${styles.Card}`}>
-                  <textarea className={styles.TextArea}></textarea>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
 
@@ -180,11 +217,20 @@ export default function DetalhesProduto() {
             <span> {valueIncremetoDecremento} </span>
             <button onClick={handleIncremento}> + </button>
           </div>
-          <div className={styles.ButtonAddFooter}>
-            <button>
-              Adicionar<span> 0 </span>
-            </button>
-          </div>
+
+          {listProduct && (
+            <div className={styles.ButtonAddFooter}>
+              <button>
+                Adicionar
+                <span>
+                  {totalPrice.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </span>
+              </button>
+            </div>
+          )}
         </div>
       </footer>
     </main>
